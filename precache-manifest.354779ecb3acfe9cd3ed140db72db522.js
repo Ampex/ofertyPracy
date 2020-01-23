756bdfae44f5ec6666e8563ab2b0af16867b2957
Ampex <kasperski.blazej@gmail.com>
2020-01-23 16:30:58 +0100

    Updates

---
+++ precache-manifest.354779ecb3acfe9cd3ed140db72db522.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4209ffbdaf1b80d107cf0bd5cef400f",
+    "url": "/ofertyPracy/index.html"
+  },
+  {
+    "revision": "743bb7e23e2521d910f8",
+    "url": "/ofertyPracy/static/css/main.cc1c10b2.chunk.css"
+  },
+  {
+    "revision": "46e97b7eb379a3a68f31",
+    "url": "/ofertyPracy/static/js/2.fdd99e53.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/ofertyPracy/static/js/2.fdd99e53.chunk.js.LICENSE"
+  },
+  {
+    "revision": "743bb7e23e2521d910f8",
+    "url": "/ofertyPracy/static/js/main.993f256b.chunk.js"
+  },
+  {
+    "revision": "ba4047a484a981319f07",
+    "url": "/ofertyPracy/static/js/runtime-main.76268a15.js"
+  },
+  {
+    "revision": "623b6c990ee67847cd80bb35162f3763",
+    "url": "/ofertyPracy/static/media/01.623b6c99.png"
+  },
+  {
+    "revision": "b864ca5d15af4a100318148c2805ce76",
+    "url": "/ofertyPracy/static/media/02.b864ca5d.jpg"
+  },
+  {
+    "revision": "917d7150818eabfaeb54f7eb1e3cf8a5",
+    "url": "/ofertyPracy/static/media/03.917d7150.jpg"
+  },
+  {
+    "revision": "5542b35e1501d2a0e0be8f5457e193d5",
+    "url": "/ofertyPracy/static/media/04.5542b35e.png"
+  },
+  {
+    "revision": "75aa8ca979ab8ebc263b3dfb3eca4d5a",
+    "url": "/ofertyPracy/static/media/06.75aa8ca9.jpg"
+  },
+  {
+    "revision": "46a1cd67e14df40c072ba2f6db3dbd66",
+    "url": "/ofertyPracy/static/media/07.46a1cd67.jpg"
+  },
+  {
+    "revision": "b40bb55f5f2ae2821f5697e9f50ae8bb",
+    "url": "/ofertyPracy/static/media/08.b40bb55f.jpg"
+  },
+  {
+    "revision": "279e4b8718024f1009cd34cc811f219c",
+    "url": "/ofertyPracy/static/media/2213285.279e4b87.jpg"
+  },
+  {
+    "revision": "8bba119ca41eee4985849a38540cfbb0",
+    "url": "/ofertyPracy/static/media/2268085.8bba119c.jpg"
+  },
+  {
+    "revision": "4a004c1c377e27f52a5fa26b5a7d8c62",
+    "url": "/ofertyPracy/static/media/bg.4a004c1c.svg"
+  },
+  {
+    "revision": "6c77d963796ea14964ee52f2d74609a1",
+    "url": "/ofertyPracy/static/media/bg.6c77d963.ai"
+  },
+  {
+    "revision": "43356f913dc009c01206c4d7766879b5",
+    "url": "/ofertyPracy/static/media/brand-logo.43356f91.ai"
+  },
+  {
+    "revision": "e6d400f51c664794b7b55b19d396d3e4",
+    "url": "/ofertyPracy/static/media/brand-logo.e6d400f5.svg"
+  }
+]);
